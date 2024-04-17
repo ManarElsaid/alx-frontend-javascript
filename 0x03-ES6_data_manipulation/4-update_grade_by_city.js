377d9883a488187bb935e20555da3a175c214d60
ManarElsaid <elsaidmanar@gmail.com>
2024-04-17 02:14:17 +0200

    6

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,20 +1,16 @@
 export default function updateStudentGradeByCity(list, city, newGrade) {
-    return list
-      .filter((obj) => obj.location === city)
-      .map((student) => {
-      //   console.log(newGrade);
-        newGrade.map((studentGrade) => {
-          if (studentGrade.studentId === student.id) {
-            // eslint-disable-next-line no-param-reassign
-            student.grade = studentGrade.grade;
-          }
-  
-          if (!student.hasOwnProperty('grade')) {
-            student.grade = 'N/A';
-          }
-          return student;
-        });
-  
+  return list
+    .filter((obj) => obj.location === city)
+    .map((student) => {
+      newGrade.map((studentGrade) => {
+        if (studentGrade.studentId === student.id) {
+          student.grade = studentGrade.grade;
+        }
+        if (!student.hasOwnProperty('grade')) {
+          student.grade = 'N/A';
+        }
         return student;
-      });
-  }
+    });
+      return student;
+    });
+}
